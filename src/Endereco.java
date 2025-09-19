77dd0b9a34b77ac0abb895c677dec149f0a39059
laviniarossito <laviniarossito.etec@gmail.com>
2025-09-19 10:13:23 -0300

    ADICIONADO: Classes de endereço

--- src/Endereco.java
+++ src/Endereco.java
@@ -2,8 +2,41 @@
 public class Endereco {
 
 	//Atributos
-	String rua;
-	String cidade;
-	String estado;
+	String ruaAt;
+	String cidadeAt;
+	String estadoAt;
+
+	//Construtores
+	public Endereco(String ruaPar, String cidadePar, String estadoPar){
+		this.ruaAt = ruaPar;
+		this.cidadeAt = cidadePar;
+		this.estadoAt = estadoPar;
+	}
+	
+	//Setters
+	public void setRua(String ruaPar){
+		this.ruaAt = ruaPar;
+	}
+
+	public void setCidade(String cidadePar){
+		this.cidadeAt = cidadePar;
+	}
+	
+	public void setEstado(String estadoPar){
+		this.estadoAt = estadoPar;
+	}
+	
+	//Getters
+	public String getRua(){
+		return ruaAt;
+	}
+	
+	public String getCidade(){
+		return cidadeAt;
+	}
+
+	public String getEstado(){
+		return estadoAt;
+	}
 
 }
