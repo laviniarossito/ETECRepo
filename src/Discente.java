eec8f92f285f68af437920a50562d75e51098058
laviniarossito <laviniarossito.etec@gmail.com>
2025-09-19 10:21:46 -0300

    ADICIONADO: Classes discente

--- src/Discente.java
+++ src/Discente.java
@@ -1,4 +1,14 @@
 
-public class Discente {
+public class Discente extends Pessoa {
 
+	//Atributos
+	int nota1At;
+	int nota2At;
+	int nota3At;
+	int nota4At;
+	
+	public Discente(int nota1Par, int nota2Par, int nota3Par, int nota4Par){
+		super();
+		this.nota1At = nota1Par;
+	}
 }
